3217ab8330a340917f85435cdaf8313391ccbd5e
ADonovanWP <anthonyrdonovan1@gmail.com>
2023-09-18 02:21:43 -0500

    Went over Values and Variables, also had an assignment to compete as well on same subject

---
+++ 09-values-varibles/main.js
@@ -0,0 +1,26 @@
+let js = "amazing";
+console.log(4 + 8 + 23 - 10);
+
+console.log("Doug");
+
+let firstName = "Gary";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let gary_newman = "JM"; // DON'T DO THIS
+let $function = 27;
+
+let person = "Walter";
+let PI = 3.1415;
+
+// Make variables descriptive
+
+// DO THIS
+let myFirstJob = "Paper Route";
+let myCurrentJob = "Developer";
+
+// NOT THIS
+let job1 = "Paper Route";
+let job2 = "Developer";
