38b9b85ab3511f28e79f95318242fceb7786f379
ADonovanWP <anthonyrdonovan1@gmail.com>
2023-09-21 23:29:18 -0500

    Went over statements and expressions

---
+++ 27-statements-expressions/main.js
@@ -0,0 +1,18 @@
+/*
+
+
+* STATEMENTS AND EXPRESSIONS
+
+
+*/
+
+3 + 4;
+1991;
+true && false && !false;
+
+if (23 > 10) {
+  const str = "23 is bigger";
+}
+
+const me = "Fred";
+console.log(`I'm ${2037 - 1987} years old ${me}`);
